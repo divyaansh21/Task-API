3b3a64946335e610d905a6098feb8dde6eb5d8b4
divyaansh21 <divyaanshmonga05@gmail.com>
2025-12-02 00:17:59 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,5 +7,5 @@ flaskAppInstance=Flask(__name__)
 if(__name__=="__main__"):
 	logger.debug("Starting the app")
 	from api import *
-	flaskAppInstance.run(host="0.0.0.0",port=5000,debug=True,use_reloader=True)
+	flaskAppInstance.run(host="0.0.0.0",port=5000,debug=False)
  
