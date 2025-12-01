a1325029158e8965e02227f4e7c9f58db06fa0cd
divyaansh21 <divyaanshmonga05@gmail.com>
2025-12-02 00:24:13 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,6 +4,10 @@ logger.basicConfig(level="DEBUG")
 
 flaskAppInstance=Flask(__name__)
 
+@flaskAppInstance.route('/')
+def home():
+    return "<h1>Project Aether is Live!</h1>", 200
+
 if(__name__=="__main__"):
 	logger.debug("Starting the app")
 	from api import *
